bbb863a7d4df6df3f037af1b8da1c15ba576c1aa
ISC-Armando <armandoalvarez1407@gmail.com>
2024-05-10 16:44:44 -0600

    Fin seccion 4 - Router

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { GraficaComponent } from './grafica/grafica.component';
+
+const router: Routes = [
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica', component: GraficaComponent }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(router)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
